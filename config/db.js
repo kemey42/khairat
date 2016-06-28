f05f1cc113b75652cd34e4d9513d8307d9e0f1f9
akmalzamel <akmalzamel@gmail.com>
2016-06-28 22:56:09 +0800

    Configured for heroku

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	url : 'mongodb://localhost:27017/khairat-dev'
+	// url : 'mongodb://localhost:27017/khairat-dev'
+  url : 'mongodb://admin:0000@ds023714.mlab.com:23714/khairat-dev'
 }
